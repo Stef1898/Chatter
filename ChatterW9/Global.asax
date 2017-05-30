fbedde86b0076472a249c6b4b4d7f15aea1c4877
Stef1898 <Stefah19@outlook.com>
2017-05-30 13:35:15 -0400

    initial commit

---
+++ ChatterW9/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChatterW9.MvcApplication" Language="C#" %>
---
+++ ChatterW9/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatterW9.Startup))]
+namespace ChatterW9
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
